4357b0137eebca86f686da4ce0141fc8c4a9b3b3
doxterpepper <dockconnellyoneal@gmail.com>
2016-04-16 16:51:13 -0400

    On my way, the math seems to have worked out and now I'm implementing it

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,36 +1,40 @@
-import MyMath
-import cmath
-import random
-import sys
+import numpy
 
 class NeuralNet:
 	def __init__(self, netSpec):
-		random.seed(1)
-		self.net = list()
+		self.layers = list()
 		for i in range(1, len(netSpec)):
-			newMat = list()
-			for j in range(netSpec[i-1]):
-				newMat.append([])
-				for k in range(netSpec[i]):
-					newMat[j].append(random.random() / 10)
-			self.net.append(MyMath.matrix(newMat))
-	
+			self.layers.append(numpy.random.rand(netSpec[i-1], netSpec[i]) * .1)
+			
 	def sigmoid(x):
-		return MyMath.matrix.invElements(1 + cmath.e ** -x)
+		return 1 / (1 + numpy.e ** -x) # This does not work...
 	
 	def dsigmoid(x):
-		s = sigmoid(x)
+		s = NeuralNet.sigmoid(x)
 		return s * (1 - s)
 	
 	def train(self, data):
-		for ds in data:
-			# Forward feed
-			output = MyMath.matrix([ds[0]]) # Data should be a vector
-			for syn in self.net:
-				output = output * syn
-			# Back propagate
-			error = list()
-			for val in output:
-				error.append(dsigmoid(val) * (ds[1] - val)
+		for ds, y in data:
+			a = list([ds])
+			# Forward Feed
+			for w in self.layers:
+				i = numpy.dot(a[-1], w)
+				a.append(NeuralNet.sigmoid(i))
+			# Back Propagate
+			init = list()
+			for i in range(self.layers[-1].shape[1]):
+				row = self.layers[-1].T[i]
+				init.append((y[i] - a[-1][i]) * NeuralNet.dsigmoid(numpy.dot(a[-2], row[i])))
+			deltas = [numpy.array(init)]
+			l = self.layers
+			l.pop()
+			l.reverse()
+			ta = a
+			ta.pop()
+			ta.reverse()
+			for w in l:
+				pass
+			exit(1)
 if __name__ == '__main__':
 	nn = NeuralNet([6, 4, 5])
+	print(nn.layers[1].shape)
